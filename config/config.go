82f1d328ee4b117debc3027048042b7936c92ce0
IwamotoKosuke <iwamoto.dengana@gmail.com>
2020-09-24 22:00:10 +0900

    first commit

--- config/config.go
+++ config/config.go
@@ -7,23 +7,25 @@ import (
 )
 
 type ConfigList struct {
-	ApiKey string
-	ApiSecret string
-	LogFile string
+	ApiKey      string
+	ApiSecret   string
+	LogFile     string
+	ProductCode string
 }
 
 var Config ConfigList
 
-func init(){
+func init() {
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
-		log.Printf("Failed to read file. %v", err)
+		log.Printf("Failed to read file: %v", err)
 		os.Exit(1)
 	}
 
 	Config = ConfigList{
-		ApiKey: cfg.Section("bitflyer").Key("api_key").String(),
-		ApiSecret: cfg.Section("bitflyer").Key("api_secret").String(),
-		LogFile: cfg.Section("gotrading").Key("log_file").String(),
+		ApiKey:      cfg.Section("bitflyer").Key("api_key").String(),
+		ApiSecret:   cfg.Section("bitflyer").Key("api_secret").String(),
+		LogFile:     cfg.Section("gotrading").Key("log_file").String(),
+		ProductCode: cfg.Section("gotrading").Key("product_code").String(),
 	}
 }
